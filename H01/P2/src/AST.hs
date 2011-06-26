2a6b9d506f8910674a60d2998e5e47e9f676a257
José Lezama <jlego@hironobu.(none)>
2011-06-26 03:53:50 -0430

    Mejorado AST, se le coloco el deriving show, y header de archivos

--- H01/P2/src/AST.hs
+++ H01/P2/src/AST.hs
@@ -1,17 +1,56 @@
 {------------------------------------- 
   Estos son los tipos que definimos   
 ---------------------------------------}
+ {-|
+   Asunto: C&#243;digo fuente del proyecto 1. 
+   Proyecto: Analizador Lexicogr&#225;fico en Haskell
+   Materia: Taller de Traductores (CI-3725)
+   Trimestre: Abril-Julio 2011
+   Profesor: Carlos Colmenares
+   
+   Grupo: H01
+   Integrantes:            Carnet:  
+   * Juan Rosas           (07-41502)
+   * Jos&#233; Lezama     (07-41104)
+   
+   Analizador Lexicogr&#225;fico desarrollado en Alex (A lexical analyser 
+   generator for Haskell) para el lenguaje Vectorinox, basado en la
+   especificaci&#243;n suministrada para el desarrollo del mismo
+   
+ -}
+
+
+module AST (
+            Raiz(..),
+            FunDec(..),
+            Vartype(..),
+            VarDec(..),
+            Instruc(..),
+            Cond2(..),
+            Print(..),
+            Exp(..),
+            Bool(..),
+            Reserve(..),
+            Function(..),
+            LeftVal(..),
+) where
+
+import Lexer
 
 data AST = Raiz
 data Raiz = [FunDec] Instruc
+	deriving (Eq, Show)
 
 data FunDec = Func String [VarDec] Vartype Instruc
+	deriving (Eq, Show)
             
 data Vartype = Num
              | Vec
              | Mat
+	deriving (Eq, Show)
 
-data VarDec = Var String Vartype
+data VarDec = Var String Vartyp
+	deriving (Eq, Show)
 
 Instruc = Asign LeftVal Exp
         | InsBlock [VarDec] [Instruc]
@@ -21,12 +60,15 @@ Instruc = Asign LeftVal Exp
         | Write [Print]
         | Return Exp
         | Cond Condition Instruct Cond2
+	deriving (Eq, Show)
 
 data Cond2 = Nothing 
            | Else Instruct
+	deriving (Eq, Show)
         
 data Print = Exp
            | Chars String
+	deriving (Eq, Show)
 
 data Exp = Num Double
          | Mat String
@@ -36,18 +78,23 @@ data Exp = Num Double
 --         | OnceA String Exp
          | Reserve 
          | Function
+	deriving (Eq, Show)
 
 data Bool = Reserve
           | Binary String Bool Bool
           | OnceD String Bool
 --          | OnceA String Bool
+	deriving (Eq, Show)
             
 data Reserve = Res String
+	deriving (Eq, Show)
 
 data Function = Func String [Exp]
+	deriving (Eq, Show)
 
 data LeftVal = Access
              | Access Exp
              | Access Exp Exp
              | Access (Just Exp) (Just Exp)
              | Access (Just Exp) (Just Exp) (Just Exp) (Just Exp)
+	deriving (Eq, Show)
