e710ea1f6d562d632d97fb2b61329f176acf3dbf
Jose Lezama <mistwalker.jl@gmail.com>
2011-07-16 20:04:03 -0430

    SymTable con funciones exportadas!

--- H01/H01/src/SymTable.hs
+++ H01/H01/src/SymTable.hs
@@ -1,37 +1,42 @@
 {-|
-  
-  Asunto: C&#243;digo fuente del Proyecto &#250;nico (Entrega 3). 
+
+  Asunto: C&#243;digo fuente del Proyecto &#250;nico (Entrega 3).
   Proyecto: Analizador Lexicogr&#225;fico en Haskell
   Materia: Taller de Traductores (CI-3725)
   Trimestre: Abril-Julio 2011
   Profesor: Carlos Colmenares
-  
+
   Grupo: H01
- 
+
  -}
 
 module SymTable (
    SymTable(..),
    Symbol(..),
+   emptySymTable,
+   find,
+   insert,
+   replace,
+   update
    ) where
 
 import qualified Data.Map as Map
 import AST
 
--- | SymTable es el tipo especificado para la entrega de este proyecto, para 
--- | expresar la Tabla de Símbolos del proyecto, funciones y los distintos 
--- | bloques del mismo, el constructor es Rama, tiene una lista de Clave y 
+-- | SymTable es el tipo especificado para la entrega de este proyecto, para
+-- | expresar la Tabla de Símbolos del proyecto, funciones y los distintos
+-- | bloques del mismo, el constructor es Rama, tiene una lista de Clave y
 -- | Símbolo, seguido de una (posible) Tabla de símbolos que es la Tabla padre
 data SymTable = Rama (Map.Map String Symbol) (Maybe SymTable)
               deriving (Show)
 
 -- | Definido con el constructor Sym y basado en 2 valores, el tipo y el valor
 -- | de la clave que apunta a ellos a través del Data.Map por el que llegamos
--- | al mismo. 
+-- | al mismo.
 data Symbol = Var VarType Exp
             | Fun VarType SymTable Instruc
               deriving (Show)
-              
+
 -- | @emptySymTable@
 emptySymTable :: SymTable
 emptySymTable = Rama (Map.empty) Nothing
@@ -48,19 +53,19 @@ find key (Rama fl Nothing) = if Map.member key fl
 find key (Rama fl (Just st)) = if Map.member key fl
                                then Map.lookup key fl
                                else find key st
-                               
--- | @isMember@ Es una función que se encarga de decir si el parámetro de 
--- | entrada (una  clave), se encuentra en el bloque de variables con el cual 
+
+-- | @isMember@ Es una función que se encarga de decir si el parámetro de
+-- | entrada (una  clave), se encuentra en el bloque de variables con el cual
 -- | es ingresado en la función, en casi afirmativo devuelve @True@, en caso
 -- | contrario devolverá de manera lógica @False@
 isMember :: String       -- ^ Símbolo a buscar en la Tabla de Símbolos.
-	 -> SymTable  -- ^ Tabla de Símbolos.
-	 -> Bool      -- ^ ¿El Símbolo está en la Tabla de Símbolos?
+         -> SymTable  -- ^ Tabla de Símbolos.
+         -> Bool      -- ^ ¿El Símbolo está en la Tabla de Símbolos?
 isMember key (Rama fl Nothing) = if Map.member key fl
-                                 then True 
+                                 then True
                                  else False
 isMember key (Rama fl (Just st)) = if Map.member key fl
-                                  then True 
+                                  then True
                                   else there
                                       where there = isMember key st
 
@@ -77,11 +82,11 @@ insert key symb (Rama fl st) = if Map.member key fl
                                else Rama (Map.insert key symb fl) st
 
 -- | @replace@ se encarga de sustituir el símbolo de una clave perteneciente a
--- | la tabla de símbolos que le es suministrada. 
+-- | la tabla de símbolos que le es suministrada.
 replace :: String   -- ^ Símbolo cuyo valor se quiere modificar.
-	-> Symbol   -- ^ Nuevo valor asociado al símbolo.
-	-> SymTable -- ^ Tabla de Símbolos a modificar.
-	-> SymTable -- ^ Tabla de Símbolos después de la modificación.
+        -> Symbol   -- ^ Nuevo valor asociado al símbolo.
+        -> SymTable -- ^ Tabla de Símbolos a modificar.
+        -> SymTable -- ^ Tabla de Símbolos después de la modificación.
 replace key symb (Rama fl st) = if Map.member key fl
                                then Rama (Map.insert key symb fl) st
                                else Rama fl st
